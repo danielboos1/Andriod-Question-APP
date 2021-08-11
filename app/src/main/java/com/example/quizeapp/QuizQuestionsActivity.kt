2ad81d527dda8b9582a2532cfd3a3fb4884f14b7
Legacy <46797782+danielboos1@users.noreply.github.com>
2021-08-11 21:47:21 +1000

    bug fixes and working on implementation of result screen

--- app/src/main/java/com/example/quizeapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizeapp/QuizQuestionsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.quizeapp
 
+import android.R.attr
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import android.media.Image
@@ -10,14 +12,20 @@ import android.view.View
 import android.widget.*
 import androidx.core.content.ContextCompat
 import org.w3c.dom.Text
+import java.util.jar.Attributes
+import android.R.attr.button
+
+
+
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var mCurrentPosition:Int = 1
-    private var mQuestionsList:ArrayList<Question>? = null
-    private var mSelectedOptionPosition : Int = 0
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
     private var mCorrectAnswers: Int = 0
-    val tv_option_one: TextView by lazy {findViewById<TextView>(R.id.tv_option_one)}
+    private var mUserName: String? = null
+    val tv_option_one: TextView by lazy { findViewById<TextView>(R.id.tv_option_one) }
     val tv_option_two: TextView by lazy { findViewById<TextView>(R.id.tv_option_two) }
     val tv_option_three: TextView by lazy { findViewById<TextView>(R.id.tv_option_three) }
     val tv_option_four: TextView by lazy { findViewById<TextView>(R.id.tv_option_four) }
@@ -27,30 +35,34 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        mUserName = intent.getStringExtra(Constants.USER_NAME)
+
         mQuestionsList = Constants.getQuestions()
 
-      setQuestion()
-        tv_option_one.setOnClickListener(this)
-        tv_option_two.setOnClickListener(this)
-        tv_option_three.setOnClickListener(this)
-        tv_option_four.setOnClickListener(this)
-        btn_sumbit.setOnClickListener(this)
+        setQuestion()
+
+            tv_option_one.setOnClickListener(this)
+            tv_option_two.setOnClickListener(this)
+            tv_option_three.setOnClickListener(this)
+            tv_option_four.setOnClickListener(this)
+            btn_sumbit.setOnClickListener(this)
     }
-    private fun setQuestion(){
+
+    private fun setQuestion() {
 
 
-        val question = mQuestionsList!![mCurrentPosition-1]
+        val question = mQuestionsList!![mCurrentPosition - 1]
         val mProgress = findViewById<ProgressBar>(R.id.progressBar)
 
         defaultOptionsView()
 
-        if(mCurrentPosition == mQuestionsList!!.size){
+        if (mCurrentPosition == mQuestionsList!!.size) {
             btn_sumbit.text = "Finish"
 
-        }else{
+        } else {
             btn_sumbit.text = "Submit"
-        }
 
+        }
         mProgress.progress = mCurrentPosition
         val Tv_Progress = findViewById<TextView>(R.id.tv_progress)
         Tv_Progress.text = "$mCurrentPosition" + "/" + mProgress.max
@@ -65,19 +77,20 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tv_option_three.text = question.optionThree
         tv_option_four.text = question.optionFour
     }
-    private fun defaultOptionsView(){
+
+    private fun defaultOptionsView() {
         val options = ArrayList<TextView>()
         options.add(0, tv_option_one)
         options.add(1, tv_option_two)
         options.add(2, tv_option_three)
         options.add(3, tv_option_four)
 
-        for (option in options){
+        for (option in options) {
             option.setTextColor(Color.parseColor("#7A8089"))
-             option.typeface = Typeface.DEFAULT
+            option.typeface = Typeface.DEFAULT
             option.background = ContextCompat.getDrawable(
-                    this,
-                    R.drawable.default_option_border_bg
+                this,
+                R.drawable.default_option_border_bg
 
             )
 
@@ -111,7 +124,12 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                             setQuestion()
                         }
                         else -> {
-                            Toast.makeText(this, "you have successfully completed the quiz", Toast.LENGTH_LONG).show()
+                            var intent = Intent(this, ResultActivity::class.java)
+                            intent.putExtra(Constants.USER_NAME, mUserName)
+                            intent.putExtra(Constants.CORRECT_ANSWERS, mCorrectAnswers)
+                            intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionsList!!.size)
+                            startActivity(intent)
+                            finish()
 
                         }
 
@@ -120,55 +138,61 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
                 } else {
                     val question = mQuestionsList?.get(mCurrentPosition - 1)
-                    if(question!!.correctAnswer != mSelectedOptionPosition){
+                    if (question!!.correctAnswer != mSelectedOptionPosition) {
                         answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
-                    }else{
+                    } else {
                         mCorrectAnswers++
                     }
                     answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
-                    if (mCurrentPosition == mQuestionsList!!.size){
+                    if (mCurrentPosition == mQuestionsList!!.size) {
                         btn_sumbit.text = "Finish"
-                    }else{
+                    } else {
+
                         btn_sumbit.text = "Next Question"
 
                     }
+                }
                 mSelectedOptionPosition = 0
-                }
+
 
             }
 
         }
     }
-        private fun answerView(answer: Int, drawableView: Int) {
-            when (answer) {
-                1 -> {
-                    tv_option_one.background = ContextCompat.getDrawable(this, drawableView)
-                }
-                2 -> {
-                    tv_option_two.background = ContextCompat.getDrawable(this, drawableView)
-                }
-                3 -> {
-                    tv_option_three.background = ContextCompat.getDrawable(this, drawableView)
-                }
-                4 -> {
-                    tv_option_four.background = ContextCompat.getDrawable(this, drawableView)
-                }
+
+    private fun answerView(answer: Int, drawableView: Int) {
+        when (answer) {
+
+            1 -> {
+                tv_option_one.background = ContextCompat.getDrawable(this, drawableView)
             }
-
+            2 -> {
+                tv_option_two.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            3 -> {
+                tv_option_three.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            4 -> {
+                tv_option_four.background = ContextCompat.getDrawable(this, drawableView)
+            }
         }
+    }
 
-        private fun selectedOptionView(tv: TextView,
-                                       SelectedOptionNum: Int) {
-            defaultOptionsView()
-            mSelectedOptionPosition = SelectedOptionNum
+    private fun selectedOptionView(
+        tv: TextView,
+        SelectedOptionNum: Int
+    ) {
+        defaultOptionsView()
+        mSelectedOptionPosition = SelectedOptionNum
 
-            tv.setTextColor(Color.parseColor("#363A43"))
-            tv.setTypeface(tv.typeface, Typeface.BOLD)
-            tv.typeface = Typeface.DEFAULT
-            tv.background = ContextCompat.getDrawable(
-                    this,
-                    R.drawable.selected_option_border_bg)
-
-        }
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.typeface = Typeface.DEFAULT
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
 
     }
+}
+
--- app/src/main/java/com/example/quizeapp/ResultActivity.kt
+++ app/src/main/java/com/example/quizeapp/ResultActivity.kt
@@ -1,11 +1,34 @@
 package com.example.quizeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        @Suppress("DEPRECATION")
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        val username = intent.getStringExtra(Constants.USER_NAME)
+        val tv_name = findViewById<TextView>(R.id.tv_name)
+        tv_name.text = username
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val CorrectAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        val tv_score = findViewById<TextView>(R.id.tv_score)
+        tv_score.text = "Your Score is $CorrectAnswers out of $totalQuestions"
+
+        val btn_finish = findViewById<Button>(R.id.btn_finish)
+        btn_finish.setOnClickListener {
+ startActivity(Intent(this, MainActivity::class.java))
+            finish()
+
+        }
+
     }
 }
